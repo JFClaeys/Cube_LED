276f0f9a6ad806119bbf0cdd693848a3df1f9540
Jean-Francois Claeys <89261005+JFClaeys@users.noreply.github.com>
2024-12-18 09:38:41 -0500

    Wrong order numbering of columns

--- Arduino/Cube_4x4_debut/Cube_4x4_debut.ino
+++ Arduino/Cube_4x4_debut/Cube_4x4_debut.ino
@@ -57,11 +57,11 @@ This applies to Arduino Uno and Nano. Maybe other AVR boards, but those are the 
       this is how each led "column" is enumerated
         0    1    2    3
 
-        4    5    6    7
+        7    6    5    4
 
         8    9   10   11
 
-       12   13   14   15  
+       15   14   13   12  
     
     
     */
